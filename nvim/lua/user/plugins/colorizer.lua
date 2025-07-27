cc0aedf1987dc4f4b36f85ae01341f85f4942f41
Riezz0 <Riezzoab@proton.me>
2025-07-27 16:25:25 +0200

    beginning of waybar mods

---
+++ nvim/lua/user/plugins/colorizer.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      -- Customize options here, for example:
+      default_options = {
+  RGB      = true;         -- #RGB hex codes
+	RRGGBB   = true;         -- #RRGGBB hex codes
+	names    = true;         -- "Name" codes like Blue
+	RRGGBBAA = true;         -- #RRGGBBAA hex codes
+	rgb_fn   = true;         -- CSS rgb() and rgba() functions
+	hsl_fn   = true;         -- CSS hsl() and hsla() functions
+	css      = true;         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn   = true;         -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        mode = "background", -- or "virtualtext", "foreground"
+      },
+    },
+  },
+}
