d7f3432011eaac6fcbc03be7ad5658c27346e915
Riezz0 <Riezzoab@proton.me>
2025-08-03 06:38:26 +0200

    rofi theme

---
+++ .zshrc
@@ -0,0 +1,20 @@
+# Startup Commands
+#fastfetch --kitty-direct /home/$USER/.config/fastfetch/ArchLinux.png --logo-width 22 --logo-height 10 --logo-padding-top 5 --logo-padding-left 2
+
+fastfetch
+
+# Path to your Oh My Zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Theme
+ZSH_THEME="robbyrussell"
+
+# Plugins
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete autoswitch_virtualenv $plugins)
+
+# Import
+source $ZSH/oh-my-zsh.sh
+
+# aliases
+alias v="nvim"
+alias ls="exa -lag --icons"
--- setup.sh
+++ setup.sh
@@ -12,8 +12,6 @@ install_yay() {
     rm -rf /tmp/yay
     echo "yay installed successfully!"
 }
-
-# Check if yay is installed
 if ! command -v yay &> /dev/null; then
     echo "yay is not installed."
     install_yay
@@ -22,7 +20,6 @@ else
 fi
 
 #-----AUR-----#
-
 echo "Installing AUR Packages"
 sleep 3
 
@@ -59,3 +56,82 @@ yay -S --needed --noconfirm \
 	exa \
 	rofi-wayland \
   goverlay-git
+
+
+#-----Oh-My-Zsh-----#
+echo "Installing Oh-My-Zsh"
+sleep 3
+
+mkdir -p /home/$USER/dots/omz
+
+
+git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "/home/$USER/dots/omz/zsh-autosuggestions/"
+git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "/home/$USER/dots/omz/zsh-syntax-highlighting/"
+git clone "https://github.com/zdharma-continuum/fast-syntax-highlighting.git" "/home/$USER/dots/omz/fast-syntax-highlighting/"
+git clone --depth 1 -- "https://github.com/marlonrichert/zsh-autocomplete.git" "/home/$USER/dots/omz/zsh-autocomplete/"
+git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "/home/$USER/dots/omz/autoswitch_virtualenv/"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm -rf ~/.zshrc
+
+cp -r /home/$USER/dots/omz/autoswitch_virtualenv/ ~/.oh-my-zsh/custom/plugins/
+cp -r /home/$USER/dots/omz/fast-syntax-highlighting/ ~/.oh-my-zsh/custom/plugins/
+cp -r /home/$USER/dots/omz/zsh-autocomplete/ ~/.oh-my-zsh/custom/plugins/
+cp -r /home/$USER/dots/omz/zsh-autosuggestions/ ~/.oh-my-zsh/custom/plugins/
+cp -r /home/$USER/dots/omz/zsh-syntax-highlighting/ ~/.oh-my-zsh/custom/plugins/
+
+#-----Config-Symlink-----#
+echo "Symlinking Configs"
+sleep 3
+
+ln -s /home/$USER/dots/.zshrc /home/$USER/
+rm -rf /home/$USER/dots/omz/
+rm -rf /home/$USER/.config/hypr
+rm -rf /home/$USER/.config/kitty
+ln -s /home/$USER/dots/fastfetch/ /home/$USER/.config/
+ln -s /home/$USER/dots/gtk-3.0/ /home/$USER/.config/
+ln -s /home/$USER/dots/gtk-4.0/ /home/$USER/.config/
+ln -s /home/$USER/dots/hypr/ /home/$USER/.config/
+ln -s /home/$USER/dots/kitty/ /home/$USER/.config/
+ln -s /home/$USER/dots/nvim/ /home/$USER/.config/
+ln -s /home/$USER/dots/rofi/ /home/$USER/.config/
+ln -s /home/$USER/dots/scripts/ /home/$USER/.config/
+ln -s /home/$USER/dots/waybar/ /home/$USER/.config/
+ln -s /home/$USER/dots/.icons/ /home/$USER/
+ln -s /home/$USER/dots/.themes/ /home/$USER/
+
+echo "Symlinking Sys Configs"
+sleep 3
+sudo rm -rf /usr/share/icons/default
+sudo cp -r /home/$USER/dots/sys/cursors/default /usr/share/icons/
+sudo cp -r /home/$USER/dots/sys/cursors/Future-black-cursors /usr/share/icons/
+
+sudo mkdir -p /usr/share/bg/
+sudo cp -r /home/$USER/dots/hypr/bg/bg.jpg /usr/share/bg/
+sudo cp -r /home/$USER/dots/.icons/oomox-anime_room /usr/share/icons/
+sudo cp -r /home/$USER/dots/.themes/oomox-anime_room /usr/share/themes/
+
+#-----Apply-Theme-----#
+echo "Applying Theme"
+Sleep 3
+
+gsettings set org.gnome.desktop.interface cursor-theme "Future-black-cursors"
+gsettings set org.gnome.desktop.interface icon-theme "oomox-anime_room"
+gsettings set org.gnome.desktop.interface gtk-theme "oomox-anime_room"
+gsettings set org.gnome.desktop.interface font-name "Jetbrains Mono Nerd Font 11"
+gsettings set org.gnome.desktop.interface document-font-name "Jetbrains Mono Nerd Font 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "Jetbrains Mono Nerd Font 11"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Jetbrains Mono Nerd Font 11"
+sudo cp -r /home/$USER/dots/sys/lightdm/ /etc/
+
+wal -i ~/.config/hypr/bg/bg.jpg
+
+echo "Installation Complete !!!"
+echo "Rebooting The System"
+
+sleep 3
+sudo systemctl reboot
+
+
+
